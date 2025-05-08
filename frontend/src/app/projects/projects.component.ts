a460438692257e2d1bc346f14018a5a1522024f8
Ragum02 <benjamin.souiket@gmail.com>
2025-05-08 22:43:29 +0200

    delete de embla carousel, pas fou pour angular

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,24 +1,12 @@
-import { Component, effect, viewChild } from '@angular/core';
-import {
-  EmblaCarouselDirective,
-  EmblaCarouselType
-} from 'embla-carousel-angular'
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [EmblaCarouselDirective],
+  standalone: true,
+  imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  private emblaRef = viewChild<EmblaCarouselDirective>(EmblaCarouselDirective);
-
-    emblaApi?: EmblaCarouselType
-   options = { loop: true }
-
-  constructor() {
-    effect(() => {
-      this.emblaApi = this.emblaRef()?.emblaApi;
-    });
-  }
 }
