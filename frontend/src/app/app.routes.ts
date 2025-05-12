1523ae7b7467b1651b619391eff29d296cb368d4
Ragum02 <benjamin.souiket@gmail.com>
2025-05-12 12:42:10 +0200

    ajout des routes admin + fetch des projects (sujet a changement pour la version fetchBackend angular)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { ManageComponent } from './backoffice/manage/manage.component';
+import { LoginComponent } from './backoffice/login/login.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: "Accueil" , data:{ animation: 'HomePage'}},
   { path: 'projets', component: ProjectsComponent, title: "Liste des projets", data:{ animation: 'ProjectsPage'} },
-  { path: 'contact', component: ContactComponent, title: "Page de contact", data: { animation: 'ContactPage' } }
-
+  { path: 'contact', component: ContactComponent, title: "Page de contact", data: { animation: 'ContactPage' } },
+  { path: 'admin/login', component: LoginComponent, title: "Page de login", data: { animation: 'LoginPage' } },
+  { path: 'admin/manage', component: ManageComponent, title: "Page d'admin", data: { animation: 'ManagePage' } }
 ];
---
+++ frontend/src/app/backoffice/manage/manage.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-manage',
+  imports: [],
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss']
+})
+export class ManageComponent {
+
+  user = '';
+
+  constructor(private router: Router) {
+    if (this.user !== 'benjamin') {
+      this.router.navigate(['/admin/login']);
+    }
+  }
+}
