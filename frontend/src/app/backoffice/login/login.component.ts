443c83cf68795899aa606054c905639254e1bb3c
Ragum02 <benjamin.souiket@gmail.com>
2025-05-13 13:17:41 +0200

    diverse correction + mise en place du form de login

--- frontend/src/app/backoffice/login/login.component.ts
+++ frontend/src/app/backoffice/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  login = new FormGroup({
+  username: new FormControl(''),
+  password: new FormControl('')
+  })
 
+
+  submit() {
+    console.log(this.login.value)
+    return false;
+  }
 }
