8fdea1ad183653a402f0439683362ae5958d0950
Ragum02 <benjamin.souiket@gmail.com>
2025-05-13 14:58:29 +0200

    refacto footer & header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { BouncingIconsComponent } from "./core/layout/boucingicons/bouncingicons.component";
 import { BackgroundComponent } from "./core/layout/background/background.component";
+import { HeaderComponent } from "./core/layout/header/header.component";
+import { FooterComponent } from "./core/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLinkActive, RouterLink, RouterOutlet, CommonModule, BouncingIconsComponent, BackgroundComponent],
+  imports: [RouterLinkActive, RouterLink, RouterOutlet, CommonModule, BouncingIconsComponent, BackgroundComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
@@ -47,52 +49,5 @@ prepareRoute(outlet: RouterOutlet) {
   return outlet?.activatedRouteData?.['animation'];
 }
   title = "Accueil";
-
-
-  @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
-
-  duration = 0;
-  currentTime = 30;
-  volume = 0.03;
-  isPlaying = false
-
-  initAudio() {
-    const audio = this.audioPlayer.nativeElement;
-    this.duration = audio.duration;
-    audio.currentTime = this.currentTime;
-    audio.volume = this.volume;
-  }
-
-  toggleLecture() {
-    const audio = this.audioPlayer.nativeElement;
-    if (audio.paused) {
-      this.isPlaying = true;
-      audio.play();
-    } else {
-      this.isPlaying = false;
-      audio.pause();
-    }
-  }
-
-  updateTime() {
-    this.currentTime = this.audioPlayer.nativeElement.currentTime;
-  }
-
-  seek(event: Event) {
-    const value = Number((event.target as HTMLInputElement).value);
-    this.audioPlayer.nativeElement.currentTime = value;
-  }
-
-  changerVolume(event: Event) {
-    const value = Number((event.target as HTMLInputElement).value);
-    this.volume = value;
-    this.audioPlayer.nativeElement.volume = value;
-  }
-
-  formatTime(time: number): string {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60).toString().padStart(2, '0');
-    return `${minutes}:${seconds}`;
-  }
 
 }
---
+++ frontend/src/app/core/layout/footer/footer.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+@ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
+
+  duration = 0;
+  currentTime = 30;
+  volume = 0.03;
+  isPlaying = false
+
+  initAudio() {
+    const audio = this.audioPlayer.nativeElement;
+    this.duration = audio.duration;
+    audio.currentTime = this.currentTime;
+    audio.volume = this.volume;
+  }
+
+  toggleLecture() {
+    const audio = this.audioPlayer.nativeElement;
+    if (audio.paused) {
+      this.isPlaying = true;
+      audio.play();
+    } else {
+      this.isPlaying = false;
+      audio.pause();
+    }
+  }
+
+  updateTime() {
+    this.currentTime = this.audioPlayer.nativeElement.currentTime;
+  }
+
+  seek(event: Event) {
+    const value = Number((event.target as HTMLInputElement).value);
+    this.audioPlayer.nativeElement.currentTime = value;
+  }
+
+  changerVolume(event: Event) {
+    const value = Number((event.target as HTMLInputElement).value);
+    this.volume = value;
+    this.audioPlayer.nativeElement.volume = value;
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  }
+}
