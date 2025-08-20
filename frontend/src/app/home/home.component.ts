ff3b6e25848d82fc01f55791fe94b031c05316fd
Ragum02 <benjamin.souiket@gmail.com>
2025-08-20 22:36:00 +0200

    style: change navbar buttons & homepage content text

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-}
+export class HomeComponent {}
